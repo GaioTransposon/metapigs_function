2160238dc7205ac5020f2110a93d9d3a79c9bc48
GaioTransposon <gaio.transposon@gmail.com>
2023-01-06 16:00:41 +0100

    normalize_counts finalized for UTS HPC

--- scripts/normalize_counts.py
+++ scripts/normalize_counts.py
@@ -5,18 +5,22 @@ Created on Mon Dec 19 16:44:44 2022
 
 @author: dgaio
 """
+
 import os
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
-mypath = "/Users/dgaio/Desktop/contigs"
-
-
 import glob
+
+
+
+mypath = input('Please pass path to directory containing files ending with "contig_Counts_parsed_weighted_contigs.csv" \n') 
+# /Users/dgaio/Desktop/contigs     <-- local UZH
+# /shared/homes/152324/contigs     <-- HPC UTS
+
+print('this is your path', mypath)
+
 filelist = glob.glob(mypath) 
-
-
-
 
 dictionary = {}   
 # list files 
@@ -25,10 +29,12 @@ for file in os.listdir(mypath):
         
         # get pig ID 
         key = file.split('_')[1]
+        print(key)
         group = dictionary.get(key,[])
         group.append(file)
         dictionary[key] = group
-        
+
+
 for my_key in dictionary:   
     
     # create empty list to accomodate dataframes
@@ -54,11 +60,12 @@ for my_key in dictionary:
     # concatenate dataframes from the same key (pigID)
     appended_data = pd.concat(appended_data, ignore_index=True)
     
-    # write to file
-    appended_data.to_csv(os.path.join(mypath, my_key),index=False)
+    this_name='counts_normalized_'+my_key
+    print(this_name)
     
-        
-print(appended_data)
+    # write to file
+    appended_data.to_csv(os.path.join(mypath, this_name),index=False)
+    
         
         
 
