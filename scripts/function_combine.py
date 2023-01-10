086319e2b03b79b4bdf69c371e38a6fc01697db4
GaioTransposon <gaio.transposon@gmail.com>
2023-01-10 17:34:37 +0100

    function_combine.py finished

--- scripts/function_combine.py
+++ scripts/function_combine.py
@@ -10,7 +10,7 @@ import os
 import pandas as pd
 
 
-path_to_contigs = input('Please pass path to contig directory" \n') 
+path_to_wa_contigs = input('Please pass path to contig directory" \n') 
 # /Users/dgaio/Desktop/contigs                                  <-- local UZH
 # /shared/homes/152324/contigs                                  <-- HPC UTS
 
@@ -27,13 +27,21 @@ type(gtdb)
 # ....
 
 
+# get list of ids
+mysamples=[]
+for k in os.listdir(path_to_wa_contigs):
+    if k.startswith("counts_normalized_"):
+        k = k.replace('counts_normalized_','')
+        mysamples.append(k)
 
-mysamples=["14159"]
+# positive and negative controls (because these have replicates and need therefore a different parsing)
+controls=['Protexin','ColiGuard','MockCommunity','NegativeControl']
+
 
 for mysample in mysamples:
 
     # open abundance file 
-    counts=path_to_contigs+"/counts_normalized_"+mysample
+    counts=path_to_wa_contigs+"/counts_normalized_"+mysample
     # read in 
     counts = pd.read_csv(counts)
     counts['bin'] = counts['bin'].astype(str)
@@ -41,34 +49,48 @@ for mysample in mysamples:
     print(len(counts))
     type(counts)
     
+    
+    if mysample in controls:
+    
+        counts[['pig', 'replicate']] = counts['pig'].str.split('_', expand=True)
+    
+
     # join 
-    df = pd.merge(counts,gtdb,on=['bin','pig'])
-    # This will give you everything in counts + add that one corresponding column in gtdb that you want to join.
+    df1 = pd.merge(counts,gtdb,on=['bin','pig'], how="left") # "left" keeps everything in counts 
+    
+    for i in df1:
+        print(i)
 
 
 
-
-
-
-    recognizer=path_to_contigs+"/prodigal/reCOGnizer_results/"+mysample+".faa/reCOGnizer_results_eval_filtered.csv"
+    recognizer=path_to_wa_contigs+"/prodigal/reCOGnizer_results/"+mysample+".faa/reCOGnizer_results_eval_filtered.csv"
     # read in
     recognizer = pd.read_csv(recognizer)
+    recognizer['pig'] = recognizer['pig'].astype(str)
     print(len(recognizer))
+    
+    for i in recognizer:
+        print(i)
+        
+        
+    print(len(recognizer))
+    print(len(df1))
 
-    df_merged1 = pd.merge(gtdb,counts, )
+
+    df2 = pd.merge(recognizer,df1, on=['contig','pig'])
+    df2
+    print(len(df2))
     
     
+    if mysample in controls:
     
-    
-    df_merged = pd.merge(recognizer,counts)
-    print(df_merged)
-    print(len(df_merged))
-    
-    
-    # # write 
-    # name_of_file=path_to_contigs+"/prodigal/reCOGnizer_results/"+mysample+"/reCOGnizer_results_eval_filtered_final.txt"
-    # df_final.to_csv(name_of_file, index=False) 
-    # print('writing done')
+        df2['pig'] = df2['pig'].astype(str) + '_' + df2['replicate'].astype(str)
+
+
+    # write 
+    name_of_file=path_to_wa_contigs+"/prodigal/reCOGnizer_results/"+mysample+".faa/"+mysample+"_reCOGnizer_results_eval_filtered_final.csv"
+    df2.to_csv(name_of_file, index=False) 
+    print('writing done')
     
     
     
