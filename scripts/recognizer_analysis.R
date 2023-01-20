f3510fa7db4a5ad367d772ecda62f3837d0fdca4
GaioTransposon <gaio.transposon@gmail.com>
2023-01-20 18:36:43 +0100

    recognizer_analysis edited; new recognizer_stats

--- scripts/recognizer_analysis.R
+++ scripts/recognizer_analysis.R
@@ -4,43 +4,50 @@ library(ggplot2)
 library(ggpubr)
 library(EnvStats)
 library(purrr)
+library(data.table)
+
+start_time <- Sys.time()
 
 df <- read_csv("/Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results/14159.faa/14159_reCOGnizer_results_eval_filtered_final.csv")
 
-# reorder dates 
-df$date  = factor(df$date, levels=c("t0",
-                                    "t1", 
-                                    "t2",
-                                    "t3",
-                                    "t4",
-                                    "t5",
-                                    "t6",
-                                    "t7",
-                                    "t8",
-                                    "t9",
-                                    "t10"))
+# # reorder dates 
+# df$date  = factor(df$date, levels=c("t0",
+#                                     "t1", 
+#                                     "t2",
+#                                     "t3",
+#                                     "t4",
+#                                     "t5",
+#                                     "t6",
+#                                     "t7",
+#                                     "t8",
+#                                     "t9",
+#                                     "t10"))
 
 df0 <- df %>%
   dplyr::filter(date=="t2"|date=="t8")
 
-unique(df0$`Functional category`)
+df1 <- df0 #%>% 
+  #dplyr::filter(`Functional category`=="Carbohydrate transport and metabolism")
 
-df1 <- df0 %>% 
-  dplyr::filter(`Functional category`=="Carbohydrate transport and metabolism")
-View(df1)
-fam <- df1 %>%
-  group_by(`Protein description`) %>%
-  tally() %>%
-  arrange(desc(n))
+# fam <- df1 %>%
+#   group_by(`DB DB`) %>%
+#   tally() %>%
+#   arrange(desc(n))
+# 
+# test1=df1 %>%
+#   dplyr::select(`Protein DB`)
+# NROW(which(is.na(test1)))
+# test2=df1 %>%
+#   dplyr::select(`DB DB`)
+# NROW(which(is.na(test2)))
 
-# for each unique protein (based on Protein description), check which ones are significantly diff (ab) between dates 
-out <- split( df1 , f = df1$`Protein description` )
-NROW(out)
+# for each unique protein (based on Protein DB), check which ones are significantly diff (ab) between dates 
+out <- split( df1 , f = df1$`DB description` )
 
+#res <- map(out, ~kruskal.test(norm_mapped_wa ~ date, data = .)) 
+res <- map(out, ~wilcox.test(norm_mapped_wa ~ date, data = .)) 
 
-res <- map(out, ~kruskal.test(norm_mapped_wa ~ date, data = .)) 
 pval <- sapply(res, "[", "p.value")
-class(pval)
 
 # to df
 y <- stack(pval)
@@ -49,17 +56,57 @@ y <- y %>%
 
 y$ind=gsub(".p.value*","",y$ind)
 
-y<-as.list(y$ind)
-class(y)
-
+# y<-as.list(y$ind)
+# class(y)
+# 
 NROW(y)
+# y
+View(y)
+
+
+# save results 
+fwrite(y,"/Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results/14159.faa/14159_reCOGnizer_hits_R.tsv", sep = "\t")
+
+end_time <- Sys.time()
+end_time-start_time
+
+
+
+z <- as.data.frame(X14159_reCOGnizer_hits_py)
+colnames(z) <- c("ind","values")
+z <- z %>% dplyr::select(values,ind)
+
+View(y)
+View(z)
+
+class(z$values)
+
+zz <- anti_join(y,z,by = c("ind"))
+
+NROW(zz)
+View(zz)
+
+
+
+
+
+
+
+z[1,]==y[1,]
+
+y %>% dplyr::filter(ind=="")
+z %>% dplyr::filter(ind=="PTS sugar transporter subunit IIC.")
+
+
+
+
+
 
 
 # run through list of signif results
-df1_sig <- subset(df1, (`Protein description` %in% y))
+df1_sig <- subset(df1, (`Protein DB` %in% y))
 
-
-to_plot <- split( df1_sig , f = df1_sig$`Protein description` )
+to_plot <- split( df1_sig , f = df1_sig$`Protein DB` )
 NROW(to_plot)
 
 
@@ -78,7 +125,7 @@ for (i in 1:39){
   p1 <- z %>%
     ggplot(., aes(x=date,y=log(norm_mapped_wa))) +
     geom_boxplot()+
-    ggtitle(z$`Protein description`)+
+    ggtitle(z$`Protein DB`)+
     stat_n_text(vjust=-1)
   
   View(z)
@@ -98,11 +145,12 @@ for (i in 1:39){
 dev.off()
 
 
+View(df1)
 
+test <- df1 %>%
+  dplyr::filter(`DB description`=='2 3 bisphosphoglycerate independent phosphoglycerate mutase iPGM. The 2,3-diphosphoglycerate- independent phosphoglycerate mutase (iPGM) catalyzes the interconversion of 3-phosphoglycerate (3PGA) and 2-phosphoglycerate (2PGA). They are the predominant PGM in plants and some other bacteria, including endospore forming Gram-positive bacteria and their close relatives. The two steps catalysis is a phosphatase reaction removing the phosphate from 2- or 3-phosphoglycerate, generating an enzyme-bound phosphoserine intermediate, followed by a phosphotransferase reaction as the phosphate is transferred from the enzyme back to the glycerate moiety. The iPGM exists as a dimer, each monomer binding 2 magnesium atoms, which are essential for enzymatic activity.')
 
-
-
-
+View(test)
 
 
 
@@ -114,7 +162,7 @@ z <- do.call(rbind.data.frame, z)
 p1 <- z %>%
   ggplot(., aes(x=date,y=log(norm_mapped_wa))) +
   geom_boxplot()+
-  ggtitle(z$`Protein description`)+
+  ggtitle(z$`Protein DB`)+
   stat_n_text(vjust=-1) 
 
 p2 <- z %>%
@@ -137,7 +185,7 @@ View(xx)
 # # Connection dbcan with new data - for discussion in manuscript: 
 # # Take along all significant hits from dbcan (differentially represented CAZy between timepoints). 
 # # Save enzyme IDs to a list. Search the recognizer_results files for items of this list 
-# # and see what `Protein description` they correspond to. Do we see the same/similar trends? 
+# # and see what `Protein DB` they correspond to. Do we see the same/similar trends? 
 #   
 # library(openxlsx)
 # dbcan = read.xlsx("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8549361/bin/mgen-7-0501-s002.xlsx",sheet=2)
---
+++ scripts/recognizer_stats.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import sys
+import numpy as np
+import pandas as pd
+from scipy import stats
+import time
+import os
+
+# run example: 
+# python recognizer_stats.py /Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results t2 t8       #local UZH   
+# python recognizer_stats.py /shared/homes/152324/contigs/prodigal/reCOGnizer_results t2 t8       #UTS HPC
+
+mypath=sys.argv[1]   
+t_before=sys.argv[2]
+t_after=sys.argv[3]
+
+
+
+
+
+
+
+# mypath="/Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results"
+# t_before="t2"
+# t_after="t8"
+
+print(mypath)
+print('analysing time interval between', t_before, 'and', t_after)
+
+
+
+
+
+start = time.time()
+
+
+
+
+
+
+for my_dir in os.listdir(mypath):
+    
+    if my_dir.endswith(".faa"):
+
+        mysample = my_dir.replace(".faa", "")
+        recognizer=mypath+'/'+my_dir+'/'+mysample+'_reCOGnizer_results_eval_filtered_final.csv'
+        
+        # read in 
+        recognizer = pd.read_csv(recognizer)
+        
+        # filter by two args (dates)
+        intervals = [t_before, t_after] 
+        
+        # selecting rows based on condition 
+        recognizer_sub = recognizer[recognizer['date'].isin(intervals)] 
+
+        # split by Protein DB
+        rec = recognizer_sub.groupby('DB description')    
+        [rec.get_group(x) for x in rec.groups]
+        
+        
+
+        down_zeros=0
+        shifts=[]
+        proteins=[]
+        pvalues=[]
+
+
+
+        for name,df in rec:
+
+            #print("\t")
+            #print(name)
+            #print(len(df))
+        
+            a=df[df["date"]==t_before].norm_mapped_wa
+            b=df[df["date"]==t_after].norm_mapped_wa
+            
+            # continue if sum of interval a and b is not zero 
+            #fc=(np.mean(b)-np.mean(a))/np.mean(a)
+            if (sum(a)+sum(b))!=0:
+                
+                if (sum(a)==0 and sum(b)!=0):
+                    
+                    down_zeros+=0
+                    shift = 'up'
+                    #print('up', sum(a), "#### 1 #####", sum(b))
+                    
+                elif sum(a)!=0 and sum(b)==0:
+                    
+                    down_zeros+=1
+                    shift = 'down'
+                    #print('down', sum(a), "#### 2 #####", sum(b))
+                    
+                elif sum(a)>sum(b):
+                    
+                    down_zeros+=0
+                    shift = 'down'
+                    #print('down', sum(a), "#### 3 #####", sum(b))
+                    
+                elif sum(a)<sum(b):
+                    
+                    down_zeros+=0
+                    shift = 'up'
+                    #print('up', sum(a), "#### 4 #####", sum(b))
+                    
+                elif sum(a)==sum(b):
+                    
+                    down_zeros+=0
+                    shift = 'none'
+                    #print('none', sum(a), "#### 5 #####", sum(b))
+                          
+            else:
+                pass
+                #print("#### sum of a and b is zero ####")
+                
+                
+            s,pval=stats.ttest_ind(a, b)
+            #s,pval=scipy.stats.mannwhitneyu(a,b)
+            #s,pval = scipy.stats.kruskal(a,b)
+            
+            shifts.append(shift)
+            proteins.append(name)
+            pvalues.append(pval)            
+                    
+        if len(proteins)==down_zeros:
+            
+            # if all rows' shifts are "down_zero" don't save any hit - it means sampling did not happen at later time point 
+            pass
+            
+            
+        else: 
+            
+            # save results map 
+            results = pd.DataFrame(np.column_stack([proteins, pvalues, shifts]), 
+                                            columns=['protein', 'pvalue', 'shift'])
+            
+            results['pvalue'] = results['pvalue'].astype(float)
+            
+
+            # filter p<0.05
+            hits = results[results['pvalue'] <= 0.05] 
+
+            # write 
+            name_of_file=mypath+"/"+my_dir+"/"+mysample+"_reCOGnizer_hits_"+t_before+"_vs_"+t_after+".tsv"
+            hits.to_csv(name_of_file, index=False, sep="\t") 
+            
+            print("Filtering and writing done")
+                
+    else:
+                
+        pass
+                
+            
+
+end = time.time()
+print("Running time: ", end-start)	
+
+
+
+
+
+
+
