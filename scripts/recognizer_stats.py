f60c415f11ffc6896ab05cd575a563f1ed5e34e3
GaioTransposon <gaio.transposon@gmail.com>
2023-01-21 21:28:03 +0100

    fc added - proteins list uniq

--- scripts/recognizer_stats.py
+++ scripts/recognizer_stats.py
@@ -10,10 +10,12 @@ Created on Wed Jan 18 15:00:00 2023
 import sys
 import numpy as np
 import pandas as pd
+import scipy
 from scipy import stats
 import time
 import os
 import csv
+from collections import Counter
 
 # run example: 
 # python recognizer_stats.py /Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results t2 t8       #local UZH   
@@ -29,9 +31,9 @@ t_after=sys.argv[3]
 
 
 
-# mypath="/Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results"
-# t_before="t2"
-# t_after="t8"
+mypath="/Users/dgaio/Desktop/contigs/prodigal/reCOGnizer_results"
+t_before="t2"
+t_after="t8"
 
 print(mypath)
 print('analysing time interval between', t_before, 'and', t_after)
@@ -53,103 +55,86 @@ for my_dir in os.listdir(mypath):
     if my_dir.endswith(".faa"):
 
         mysample = my_dir.replace(".faa", "")
+        #print(mysample)
         recognizer=mypath+'/'+my_dir+'/'+mysample+'_reCOGnizer_results_eval_filtered_final.csv'
         
         # read in 
         recognizer = pd.read_csv(recognizer)
         
-        # filter by two args (dates)
-        intervals = [t_before, t_after] 
         
-        # selecting rows based on condition 
-        recognizer_sub = recognizer[recognizer['date'].isin(intervals)] 
+        # if no info on later time point, don't continue:
+        check_if_any = recognizer[recognizer["date"] == t_after]
+        if len(check_if_any)==0:
+            
+            print("subject ", mysample, " was not sampled at the later time point")
+        
+        
+        # else proceed calculating shifts 
+        else: 
+
+            # filter by two args (dates)
+            intervals = [t_before, t_after] 
+            
+            # selecting rows based on condition 
+            recognizer_sub = recognizer[recognizer['date'].isin(intervals)] 
+    
+            # split by Protein DB
+            rec = recognizer_sub.groupby('DB description')    
+            [rec.get_group(x) for x in rec.groups]
+            
+    
+            shifts=[]
+            proteins=[]
+            pvalues=[]
+
     
 
-        # split by Protein DB
-        rec = recognizer_sub.groupby('DB description')    
-        [rec.get_group(x) for x in rec.groups]
-        
-        
-
-        down_zeros=0
-        shifts=[]
-        proteins=[]
-        pvalues=[]
-
-
-
-        for name,df in rec:
-
-            #print("\t")
-            #print(name)
-            #print(len(df))
-        
-            a=df[df["date"]==t_before].norm_mapped_wa
-            b=df[df["date"]==t_after].norm_mapped_wa
+            for name,df in rec:
+    
+                #print("\t")
+                #print(name)
+                #print(len(df))
             
-            # continue if sum of interval a and b is not zero 
-            #fc=(np.mean(b)-np.mean(a))/np.mean(a)
-            if (sum(a)+sum(b))!=0:
-                
-                if (sum(a)==0 and sum(b)!=0):
-                    
-                    down_zeros+=0
-                    shift = 'up'
-                    #print('up', sum(a), "#### 1 #####", sum(b))
-                    
-                elif sum(a)!=0 and sum(b)==0:
-                    
-                    down_zeros+=1
-                    shift = 'down'
-                    #print('down', sum(a), "#### 2 #####", sum(b))
-                    
-                elif sum(a)>sum(b):
-                    
-                    down_zeros+=0
-                    shift = 'down'
-                    #print('down', sum(a), "#### 3 #####", sum(b))
-                    
-                elif sum(a)<sum(b):
-                    
-                    down_zeros+=0
-                    shift = 'up'
-                    #print('up', sum(a), "#### 4 #####", sum(b))
-                    
-                elif sum(a)==sum(b):
-                    
-                    down_zeros+=0
-                    shift = 'none'
-                    #print('none', sum(a), "#### 5 #####", sum(b))
-                          
-            else:
-                pass
-                #print("#### sum of a and b is zero ####")
+                a=df[df["date"]==t_before].norm_mapped_wa
+                b=df[df["date"]==t_after].norm_mapped_wa
                 
                 
-            s,pval=stats.ttest_ind(a, b)
-            #s,pval=scipy.stats.mannwhitneyu(a,b)
-            #s,pval = scipy.stats.kruskal(a,b)
-            
-            shifts.append(shift)
-            proteins.append(name)
-            pvalues.append(pval)     
-            
-            ##########
-            # save proteins into list if not already present: 
-            if name not in proteins_big:
-                proteins_big.append(name)
-            else:
-                pass
-            ##########            
-            
+                try:
                     
-        if len(proteins)==down_zeros:
+                    fc=(np.mean(b)-np.mean(a))/np.mean(a)
+                    
+                except ZeroDivisionError:
+                    
+                    
+                    if np.mean(a)>np.mean(b):
+                        
+                        #print("to zero")
+                        fc=float("NaN")
+                        
+                    elif np.mean(a)<np.mean(b):
+                        
+                        #print("from zero")
+                        fc=float("NaN")
+                        
+                    else:
+                        
+                        #print("zeros")
+                        fc=0
+                        
+
+                s,pval=stats.ttest_ind(a, b)
+                #s,pval=scipy.stats.mannwhitneyu(a,b)
+                #s,pval = scipy.stats.kruskal(a,b)
+                
+    
+                shifts.append(fc)
+                proteins.append(name)
+                pvalues.append(pval)     
+                len(shifts)
+                len(proteins)
+                len(pvalues)
             
-            # if all rows' shifts are "down_zero" don't save any hit - it means sampling did not happen at later time point 
-            pass
-            
-            
-        else: 
+
             
             # save results map 
             results = pd.DataFrame(np.column_stack([proteins, pvalues, shifts]), 
@@ -160,6 +145,15 @@ for my_dir in os.listdir(mypath):
 
             # filter p<0.05
             hits = results[results['pvalue'] <= 0.05] 
+            
+            ##########
+            # save names of proteins that were found significantly changed between time points, in list (if not already present) 
+            for p in hits['protein']:
+                if p not in proteins_big:
+                    proteins_big.append(p)
+                else:
+                    pass
+            ##########  
 
             # write 
             name_of_file=mypath+"/"+my_dir+"/"+mysample+"_reCOGnizer_hits_"+t_before+"_vs_"+t_after+".tsv"
@@ -175,16 +169,14 @@ for my_dir in os.listdir(mypath):
 
 
 len(proteins_big)
+
             
 # save proteins_big (list of proteins containing all unique hits among all the subjects)
-name_of_file=mypath+"/all_proteins_hits_"+t_before+"_vs_"+t_after+".txt"
-
-
-
-file = open(name_of_file,'w')
-for item in proteins_big:
-	file.write(item+"\t")
-file.close()
+name_of_file=mypath+"/all_proteins_hits_"+t_before+"_vs_"+t_after+".tsv"
+# file = open(name_of_file,'w')
+# for item in proteins_big:
+# 	file.write(item+"\t")
+# file.close()
     
     
     
