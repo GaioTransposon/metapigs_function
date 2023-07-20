23338cc12076a25407750446718f35796a6d60a4
GaioTransposon <gaio.transposon@gmail.com>
2023-07-20 13:13:11 +0200

    function riaggancio before ISCB

--- scripts/eggnogg_split_into_paths.py
+++ scripts/eggnogg_split_into_paths.py
@@ -22,33 +22,65 @@ start = time.time()
 
 #python eggnogg_split_into_paths.py 
 
-where=os.path.expanduser('~')
-path_to_egg=where+'/contigs/prodigal/eggnogg'
+my_path=os.path.expanduser('~')+'/github/metapigs_function/middle_dir'
+
+path_to_egg=os.path.expanduser('~')+'/contigs/prodigal/eggnogg'
+
 
 ##########################################################################
 
 
-# open EC to KO translations:
-filename=os.path.expanduser('~')+'/github/metapigs_function/middle_dir/'+'ec_to_ko.tsv'
-ec_to_ko = pd.read_csv(filename, index_col=None, sep='\t')
+# =============================================================================
+# # open EC to KO translations:
+# filename=os.path.expanduser('~')+'/github/metapigs_function/middle_dir/'+'ec_to_ko.tsv'
+# ec_to_ko = pd.read_csv(filename, index_col=None, sep='\t')
+# =============================================================================
 
-# open the pathways dictionary: 
-filename=os.path.expanduser('~')+'/github/metapigs_function/middle_dir/'+'pathways.pkl'
-with open(filename, 'rb') as fp:
-    my_pathways = pickle.load(fp)
-    print(my_pathways)
+# open the pathways df containing KOs for each:  
+filename=my_path+"/pathways_selection_with_KOs.csv"
+pathways_selection = pd.read_csv(filename)
 
-#my_pathways=dict(list(my_pathways.items())[0: 70])
+#pathways_selection=pathways_selection[10:12]
 
-new_pathways={}
-for i in my_pathways:
-    new_pathways[i]=[]
-    this=[]
-    for ii in my_pathways[i]:
-        ii=ii.replace('ko:','')
-        this.append(ii)
-    new_pathways[i]=this
-my_pathways=new_pathways
+
+import ast
+
+def create_value(row):
+    # Convert kos string to a list
+    kos_list = ast.literal_eval(row['kos'])
+    return [row['description']] + kos_list
+
+# Create a new column in the dataframe that contains the desired list of values
+pathways_selection['value'] = pathways_selection.apply(create_value, axis=1)
+
+# Convert the dataframe to a dictionary where 'path' is the key and 'value' is the value
+paths_dict = pathways_selection.set_index('path')['value'].to_dict()
+
+
+
+# =============================================================================
+# # open EC to KO translations:
+# filename=os.path.expanduser('~')+'/github/metapigs_function/middle_dir/'+'ec_to_ko.tsv'
+# ec_to_ko = pd.read_csv(filename, index_col=None, sep='\t')
+# 
+# # open the pathways dictionary: 
+# filename=os.path.expanduser('~')+'/github/metapigs_function/middle_dir/'+'pathways.pkl'
+# with open(filename, 'rb') as fp:
+#     my_pathways = pickle.load(fp)
+#     print(my_pathways)
+# 
+# #my_pathways=dict(list(my_pathways.items())[0: 70])
+# 
+# new_pathways={}
+# for i in my_pathways:
+#     new_pathways[i]=[]
+#     this=[]
+#     for ii in my_pathways[i]:
+#         ii=ii.replace('ko:','')
+#         this.append(ii)
+#     new_pathways[i]=this
+# my_pathways=new_pathways
+# =============================================================================
 
 
 # for each subject: 
@@ -85,9 +117,9 @@ for my_dir in os.listdir(path_to_egg):
             # sometimes >1 KOs are given. expand these rows, they won't be summed or average together anyway. 
             egg = egg.explode('KO').reset_index(drop=True)
     
-            for i in my_pathways:
-                #print(my_pathways[i])
-                these_KOs=my_pathways[i]
+            for i in paths_dict:
+                #print(paths_dict[i])
+                these_KOs=paths_dict[i]
                 
                 egg_sub=egg[egg['KO'].isin(these_KOs)]
                 egg_sub = egg_sub.copy()
--- scripts/webscrape_pathways.py
+++ scripts/webscrape_pathways.py
@@ -10,63 +10,88 @@ import os
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import urllib.request
-
-
-##############################################################################
-
-
-
 import time
 import pandas as pd
+import re
+
+
+##########################################################################
+
+my_path=os.path.expanduser('~')+'/github/metapigs_function/middle_dir'
+
+##########################################################################
+
 
 
 url = 'https://www.genome.jp/kegg-bin/show_organism?menu_type=pathway_maps&category=Prokaryotes'
 response = requests.get(url)
 
 html_content = response.content
-    
 soup = BeautifulSoup(html_content, 'html.parser')
-soup
 
-# Find all links on the page
-links = soup.find_all('ul')
-links
+# Find first two 'b' tags
+b_tags = soup.find_all('b', limit=2)
+
+# Convert the soup to string
+soup_str = str(soup)
+
+# Locate the indexes of the two 'b' tags in the string
+first_b_index = soup_str.index(str(b_tags[0]))
+second_b_index = soup_str.index(str(b_tags[1]))
+
+# Slice the soup string to only include content between these indexes
+sliced_soup_str = soup_str[first_b_index:second_b_index]
+
+# Parse the sliced soup string back into BeautifulSoup object
+sliced_soup = BeautifulSoup(sliced_soup_str, 'html.parser')
+
+# Find all 'ul' tags in the sliced soup
+links = sliced_soup.find_all('ul')
+
+# Now you have a list of all 'ul' tags between the first two 'b' tags
+for link in links:
+    print(link)
 
 
 
-
-for ul in links:
-    print(ul)
-
-
+# extract all info
 mydic={}
 for p in links:
-    
     header=p.previousSibling.text
     header=header.strip()
 
+    # Collect the map numbers and pathway names
+    pathways = p.find_all('a')
+    map_numbers_and_names = []
+    for pathway in pathways:
+        # Extract map number from the href attribute
+        map_number = re.search(r'mapno=(\d+)', pathway['href']).group(1)
+        # Extract the name of the pathway
+        name = pathway.text.strip().replace(u'\xa0', u' ')
+        map_numbers_and_names.append((map_number, name))
+
+    mydic[header] = map_numbers_and_names
     
-    mystr=p.text.strip().replace(u'\xa0', u' ')
-    mylist=mystr.splitlines()
-    
-    newlist=[]
-    for i in mylist:
-        print(i)
-        i=i.split()
-        ii=i[1:len(i)]
-        iii=' '.join(ii)
-        newlist.append(iii)
-        
-    mydic[header]=newlist
-    
-mydf=pd.DataFrame(mydic.items())
-mydf.columns = ['group', 'pathways']
-mydf=mydf.explode('pathways').reset_index(drop=True)        
+# Convert the dictionary to a DataFrame
+mydf = pd.DataFrame([
+    {'group': group, 'path': 'map'+str(mapno), 'description': pathway}
+    for group, map_numbers_and_names in mydic.items()
+    for mapno, pathway in map_numbers_and_names
+])
+
+mydf = mydf[mydf.group != '']
+mydf = mydf.reset_index(drop=True)
+
+print(mydf)
+
+
+# Save the DataFrame to a CSV file
+filename=my_path+"/pathways_selection.csv"
+mydf.to_csv(filename, index=False)
 
 
 
-mydf=mydf[mydf.group != '']
-mydf=mydf.reset_index()
+
 
 
 
